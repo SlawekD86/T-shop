234660c30b20edd482ca6c3c2da269bd2f8f7ddb
Slawek Derabin <slawomirderabin@gmail.com>
2023-03-27 13:52:57 +0200

    price and size

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,38 +1,75 @@
-import PropTypes from 'prop-types';
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const Product = ({ title, basePrice }) => {
+const Product = (props) => {
+  const [currentColor, setCurrentColor] = useState(props.colors[0]);
+  const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
+  const [currentPrice, setCurrentPrice] = useState(props.sizes[0].additionalPrice);
+
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  }
+
+  const getPrice = () => {
+    return props.basePrice + currentPrice;
+  };
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
-        <img
-          className={styles.image}
-          alt={`${title} shirt`}
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+        <img className={styles.image} alt={`${props.title} - ${currentColor} - ${currentSize}`} size={currentSize} src={`${process.env.PUBLIC_URL}/images/products/shirt-${props.name}--${currentColor}.jpg`} />
       </div>
       <div>
         <header>
-          <h2 className={styles.name}>{title}</h2>
-          <span className={styles.price}>Price: {basePrice}$</span>
+          <h2 className={styles.name}>{props.title}</h2>
+          <span className={styles.price}> {getPrice()}$</span>
         </header>
         <form>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              {props.sizes.map((size) => (
+                <li key={size.name}>
+                  <button
+                    type="button"
+                    className={clsx(
+                      styles.size,
+                      size.name === currentSize && styles.active
+                    )}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setCurrentSize(size.name);
+                      setCurrentPrice(size.additionalPrice);
+                    }}
+                  >
+                    {size.name}
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              {props.colors.map((color) => (
+                <li key={color}>
+                  <button
+                    type="button"
+                    className={clsx(
+                      prepareColorClassName(color),
+                      color === currentColor && styles.active
+                    )}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setCurrentColor(color);
+                    }}
+                  >
+                  </button>
+                </li>
+              ))}
             </ul>
           </div>
           <Button className={styles.button}>
@@ -41,11 +78,15 @@ const Product = ({ title, basePrice }) => {
         </form>
       </div>
     </article>
-  )
+  );
 };
 
 Product.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  sizes: PropTypes.array.isRequired,
+  colors: PropTypes.array.isRequired,
   basePrice: PropTypes.number.isRequired,
 };
 
